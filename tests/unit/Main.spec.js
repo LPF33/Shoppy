61d43eb79f1569f059f6cb3fadbfd56b7509af00
LPF33 <larspefe@gmail.com>
2021-08-02 17:02:43 +0200

    test(5 tests): added tests for App, Jambo, Login, Main, Nabar

---
+++ tests/unit/Main.spec.js
@@ -0,0 +1,153 @@
+import { mount, flushPromises } from "@vue/test-utils";
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "@/router/index.js";
+import Main from "@/views/Main";
+
+let $store = {};
+
+beforeEach(() => {
+  $store = {
+    state: {
+      user: "lars@gmail.com",
+    },
+    commit: jest.fn(),
+  };
+});
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+describe("Testing the Main component", () => {
+  it("Main component renders as expected when Lars is logged in", async () => {
+    router.push("/");
+    await router.isReady();
+
+    const wrapper = mount(Main, {
+      data() {
+        return {
+          publicPath: "http://localhost:3000/",
+        };
+      },
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+    const mainWrapper = wrapper.find("main");
+    expect(mainWrapper.exists()).toBeTruthy();
+    expect(mainWrapper.find("img").attributes("src")).toContain(
+      "img/wedding.JPG"
+    );
+    expect(mainWrapper.find(".header").exists()).toBeFalsy();
+    expect(mainWrapper.find(".emoji").text()).toBe("🦋");
+    expect(mainWrapper.find("aside").text()).toContain("Tage bis zur Hochzeit");
+    const footer = mainWrapper.find("footer");
+    expect(footer.exists()).toBeTruthy();
+    expect(footer.findAll("a")).toHaveLength(2);
+    expect(footer.findAll("button")).toHaveLength(1);
+  });
+
+  it("Click on Logout-Button, should call 2 $store commit functions", async () => {
+    router.push("/");
+    await router.isReady();
+
+    const wrapper = mount(Main, {
+      data() {
+        return {
+          publicPath: "http://localhost:3000/",
+        };
+      },
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    const logoutButton = wrapper.find("button");
+    await logoutButton.trigger("click");
+
+    expect($store.commit).toHaveBeenCalledTimes(2);
+    expect($store.commit.mock.calls[0][0]).toBe("unsubscribeFirebase");
+    expect($store.commit.mock.calls[1][0]).toBe("logout");
+  });
+
+  it("Main component renders as expected when not Lars is logged in", async () => {
+    $store.state.user = "not-lars@gmail.com";
+    router.push("/");
+    await router.isReady();
+
+    const wrapper = mount(Main, {
+      data() {
+        return {
+          publicPath: "http://localhost:3000/",
+        };
+      },
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    const mainWrapper = wrapper.find("main");
+    expect(mainWrapper.exists()).toBeTruthy();
+    expect(mainWrapper.find("img").attributes("src")).toContain(
+      "img/wedding.JPG"
+    );
+    expect(mainWrapper.find(".header").exists()).toBeTruthy();
+    expect(mainWrapper.find("aside").text()).toContain("Tage bis zur Hochzeit");
+    const footer = mainWrapper.find("footer");
+    expect(footer.exists()).toBeTruthy();
+    expect(footer.findAll("a")).toHaveLength(2);
+    expect(footer.findAll("button")).toHaveLength(1);
+  });
+
+  it("Click on router-link with name Budget navigates to '/budget'", async () => {
+    router.push("/");
+    await router.isReady();
+
+    const wrapper = mount(Main, {
+      data() {
+        return {
+          publicPath: "http://localhost:3000/",
+        };
+      },
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    const jamboLink = wrapper.findAll("a")[0];
+    expect(jamboLink.attributes("href")).toBe("/budget");
+    jamboLink.trigger("click");
+    await flushPromises();
+    expect(wrapper.vm.$route.path).toBe("/budget");
+  });
+
+  it("Click on router-link with name Jambo navigates to '/jambo'", async () => {
+    router.push("/");
+    await router.isReady();
+
+    const wrapper = mount(Main, {
+      data() {
+        return {
+          publicPath: "http://localhost:3000/",
+        };
+      },
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    const jamboLink = wrapper.findAll("a")[1];
+    expect(jamboLink.attributes("href")).toBe("/jambo");
+    jamboLink.trigger("click");
+    await flushPromises();
+    expect(wrapper.vm.$route.path).toBe("/jambo");
+  });
+});
---
+++ tests/unit/Navbar.spec.js
@@ -0,0 +1,155 @@
+import { mount, flushPromises } from "@vue/test-utils";
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "@/router/index.js";
+import Navbar from "@/components/Navbar";
+
+let $store = {};
+
+beforeEach(() => {
+  $store = {
+    state: {
+      error: false,
+      isAuthenticated: false,
+    },
+  };
+});
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+describe("Testing the Navbar component", () => {
+  it("When user is not logged in, Navbar should have on router-link", async () => {
+    router.push("/");
+    await router.isReady();
+    const wrapper = mount(Navbar, {
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find("nav").exists()).toBeTruthy();
+    expect(wrapper.findAll("a")).toHaveLength(1);
+    expect(wrapper.findAll("a")[0].attributes("href")).toBe("/login");
+    expect(wrapper.findAll("a")[0].text()).toBe("Login");
+  });
+
+  it("When user is logged in, Navbar should render three router-links", async () => {
+    $store.state.isAuthenticated = true;
+    router.push("/");
+    await router.isReady();
+    const wrapper = mount(Navbar, {
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find("nav").exists()).toBeTruthy();
+    expect(wrapper.findAll("a")).toHaveLength(3);
+    expect(wrapper.findAll("a")[0].attributes("href")).toBe("/");
+    expect(wrapper.findAll("a")[0].text()).toBe("Main");
+    expect(wrapper.findAll("a")[1].attributes("href")).toBe("/shoppy");
+    expect(wrapper.findAll("a")[1].text()).toBe("Shoppy");
+    expect(wrapper.findAll("a")[2].attributes("href")).toBe("/cooky");
+    expect(wrapper.findAll("a")[2].text()).toBe("Cooky");
+    expect(wrapper.findAll(".router-link-active")).toHaveLength(1);
+    expect(wrapper.find(".router-link-active").attributes("href")).toBe("/");
+  });
+
+  it("User is logged in, navigate to Shoppy-Route, Navbar should render accordingly", async () => {
+    $store.state.isAuthenticated = true;
+    router.push("/shoppy");
+    await router.isReady();
+    const wrapper = mount(Navbar, {
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+    await flushPromises();
+    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find("nav").exists()).toBeTruthy();
+    expect(wrapper.findAll("a")).toHaveLength(3);
+    expect(wrapper.findAll(".router-link-active")).toHaveLength(1);
+    expect(wrapper.find(".router-link-active").attributes("href")).toBe(
+      "/shoppy"
+    );
+  });
+
+  it("User is logged in, navigate to Cooky-Route, Navbar should render accordingly", async () => {
+    $store.state.isAuthenticated = true;
+    router.push("/cooky");
+    await router.isReady();
+    const wrapper = mount(Navbar, {
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+    await flushPromises();
+    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find("nav").exists()).toBeTruthy();
+    expect(wrapper.findAll("a")).toHaveLength(3);
+    expect(wrapper.findAll(".router-link-active")).toHaveLength(1);
+    expect(wrapper.find(".router-link-active").attributes("href")).toBe(
+      "/cooky"
+    );
+  });
+
+  it("When error occurs, div with CSS class error-nav gets rendered", async () => {
+    $store.state.error = true;
+    await router.isReady();
+
+    const wrapper = mount(Navbar, {
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find(".error-nav").exists()).toBeTruthy();
+    expect(wrapper.find(".error-nav").text()).toBe("❗");
+  });
+
+  it("Click on router-link with name Shoppy navigates to '/shoppy'", async () => {
+    $store.state.isAuthenticated = true;
+    await router.isReady();
+
+    const wrapper = mount(Navbar, {
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    const shoppyLink = wrapper.findAll("a")[1];
+    expect(shoppyLink.attributes("href")).toBe("/shoppy");
+    shoppyLink.trigger("click");
+    await flushPromises();
+    expect(wrapper.vm.$route.path).toBe("/shoppy");
+  });
+
+  it("Click on router-link with name Cooky navigates to '/cooky'", async () => {
+    $store.state.isAuthenticated = true;
+    await router.isReady();
+
+    const wrapper = mount(Navbar, {
+      global: {
+        plugins: [router],
+        mocks: { $store },
+      },
+    });
+
+    const cookyLink = wrapper.findAll("a")[2];
+    expect(cookyLink.attributes("href")).toBe("/cooky");
+    cookyLink.trigger("click");
+    await flushPromises();
+    expect(wrapper.vm.$route.path).toBe("/cooky");
+  });
+});
